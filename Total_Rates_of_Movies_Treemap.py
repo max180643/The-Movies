a68bd7013f1c690900e760da1876f4ff875c76b9
Chanwit Settavongsin <max180643@gmail.com>
2018-11-25 15:10:47 +0700

    Moved from Code Folder

---
+++ Total_Rates_of_Movies_Treemap.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import pygal
+import time
+start_time = time.time()
+
+#Banner
+print("|>>>Total Rates of Movies in year 2000-2016 Dataset<<<|")
+
+def rate_movies():
+    """ Create treemap_graph of total of movies in year 2000-2016 and Load Dataset title and rate"""
+    for year in range(2000, 2017):
+        print(">>In Year : %i" % year)
+
+        # Start display
+        print(">>[status] Create Graph Starting!")
+
+        dataset = pd.read_csv("Data_Export/%i.csv" % (year))
+        title = dataset["title"].tolist() #Title Movies
+        rate = dataset["rate"].tolist() #Rate
+        graph = pygal.Treemap(x_title="Movie", y_title="Rate")
+        graph.title = "Total Rates of Movies in %i Dataset" % year
+        for i in range(len(title)):
+            graph.add(title[i], rate[i])
+        graph.render_to_file("Graph_Export/Rates_Year/Treemap/Total_Rates_of_Movies_%i.svg" % year)
+
+        # End display
+        print(">>[status] Created Graph Successful!")
+        print()
+
+    # Used time
+    print("Completed : [status] Used time : %s seconds" % (time.time() - start_time))
+
+rate_movies()
---
+++ Total_of_Movies_in_2000 - 2016_Dataset.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pygal
+import time
+start_time = time.time()
+
+#banner
+print("[Total of Movies in 2000 - 2016 Dataset]")
+
+def treemap_graph():
+    """ Create treemap_graph of total of movies 2000 - 2016 """
+    # start display
+    print("[status] Creat Graph Starting!")
+
+    graph = pygal.Bar(x_title="Year", y_title="Total", legend_at_bottom=True, print_values=True)
+    graph.title = "Total of Movies in 2000 - 2016 Dataset"
+    for i in range(2000, 2017):
+        graph.add(str(i), dataload(i))
+    graph.render_to_file("Graph_Export/Total_of_Movies_2000-2016.svg")
+
+    # end display
+    print("[status] Created Graph Successful!")
+
+    # Used time
+    print("[status] Used time : %s seconds" % (time.time() - start_time))
+
+def dataload(year):
+    """ Load Dataset """
+    dataset = pd.read_csv("Data_Export/%i.csv" % (year))
+    total = len(dataset["title"].tolist())
+    return total
+
+treemap_graph()
