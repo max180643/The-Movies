7e57b95c97cc0a3f4b42bb5af91650415cf05618
Chanwit Settavongsin <max180643@gmail.com>
2018-11-25 17:25:16 +0700

    Renamed File

--- Number_of_Movies.py
+++ Number_of_Movies.py
@@ -4,7 +4,7 @@ import time
 start_time = time.time()
 
 #banner
-print("|>>> Total of Movies in 2000 - 2016 Dataset <<<|")
+print("|>>> Number of Movies in 2000 - 2016 Dataset <<<|")
 
 def treemap_graph():
     """ Create treemap_graph of total of movies 2000 - 2016 """
@@ -12,10 +12,10 @@ def treemap_graph():
     print(">> [status] Creat Graph Starting!")
 
     graph = pygal.Bar(x_title="Year", y_title="Total", legend_at_bottom=True, print_values=True)
-    graph.title = "Total of Movies in 2000 - 2016 Dataset"
+    graph.title = "Number of Movies by year"
     for i in range(2000, 2017):
         graph.add(str(i), dataload(i))
-    graph.render_to_file("Graph_Export/Total_of_Movies_2000-2016.svg")
+    graph.render_to_file("Graph_Export/Number_of_Movies.svg")
 
     # end display
     print(">> [status] Created Graph Successful!")
